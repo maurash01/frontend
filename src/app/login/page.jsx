390f6bbcb539f9a3210b0c15c0aa25ad7a9cd733
maurash01 <ashumaurya023@gmail.com>
2024-06-03 20:01:33 +0530

    More On React & Node.js

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import classes from './login.module.css';
 
 const login = () => {
   return (
-    <div>Login Page</div>
+    <div>
+      <h1>Login Page</h1>
+      <button className='my-btn'>Login Button</button>
+
+      <button className={ classes.btn}>Another button </button>
+      
+    </div>
   )
 }
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import logo from './vercel.svg';
 
 const Home = () => {
   return (
     <div>
       <h1 style={{textAlign:'center', fontSize:'56px', color: 'red'}}>Home Page</h1>
       <input type="text" className='border-2 border-black' />
-    
+      <button className='my-btn rounded-lg'>Submit</button>
+      <img src="/next.svg" alt="" />
+      <img src={logo.src} alt="" />
+      {/*  */}
     </div>
   )
 }
